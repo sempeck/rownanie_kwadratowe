9a92cab421e82a88ecbdb00f64054d351700f8ff
sempeck <jarek.sepek@mediorama.pl>
2015-10-08 14:49:41 +0200

    dodaje obliczenie delty

--- rownanie.pp
+++ rownanie.pp
@@ -1,13 +1,13 @@
 Program rownianie_kwadratowe;
 
 var
-  a, b, c: Real;
-  delta: Real;
-  x1, x2: Real;
+  a, b, c : Real;
+  delta : Real;
+  x1, x2 : Real;
 
 
 begin
-  WriteLn('Program oblicza wartość równania kwadratowego.')
+  WriteLn('Program rozwiązuje równanie kwadratowe metodą wyznaczników.');
 
   Write('Podaj wartość a:'); 
   ReadLn(a);
@@ -18,5 +18,13 @@ begin
   Write('Podaj wartość c:'); 
   ReadLn(c);
 
-  Write('Podałeś: ', a, b, c)
+  delta := b*b - 4*a*c;
+
+  x1 := (-b + sqrt(delta))/(2*a);
+  x2 := (-b - sqrt(delta))/(2*a);
+  
+  WriteLn('x1 to: ', x1:12:4);
+  WriteLn('x2 to: ', x2:12:4);
+
+  ReadLn;
 end.
