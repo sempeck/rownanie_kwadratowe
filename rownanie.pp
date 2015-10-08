81e8e3be8b8d1388694b922bfc8c7eb0dd9c03b0
sempeck <jarek.sepek@mediorama.pl>
2015-10-08 13:18:51 +0200

    Initial commit

---
+++ rownanie.pp
@@ -0,0 +1,22 @@
+Program rownianie_kwadratowe;
+
+var
+  a, b, c: Real;
+  delta: Real;
+  x1, x2: Real;
+
+
+begin
+  WriteLn('Program oblicza wartość równania kwadratowego.')
+
+  Write('Podaj wartość a:'); 
+  ReadLn(a);
+
+  Write('Podaj wartość b:'); 
+  ReadLn(b);
+
+  Write('Podaj wartość c:'); 
+  ReadLn(c);
+
+  Write('Podałeś: ', a, b, c)
+end.
