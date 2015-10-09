a143f7d4a9a789bd923610e297c454a050151c52
sempeck <jarek.sepek@mediorama.pl>
2015-10-09 09:20:00 +0200

    rysowanie szlaczków

--- rownanie.pp
+++ rownanie.pp
@@ -1,42 +1,28 @@
-Program rownianie_kwadratowe;
-  
-var
-  a, b, c : Real;
-  delta : Real;
-  x1, x2 : Real;
+Program choinka;
 
+
+procedure szlaczek(Znak:Char; Ile:Integer);
+  var
+  i : Integer;
+
+  begin
+    for i := 1 to Ile do
+    write(Znak);
+    WriteLn;
+  end;
+
+procedure choinka(wysokosc:Integer);
+
+var
+  k : Integer;
+
+  begin
+    for k := 1 to wysokosc do
+     szlaczek('*', k);
+  end;
 
 begin
-  WriteLn('Program rozwiązuje równanie kwadratowe metodą wyznaczników.');
-
-  Write('Podaj wartość a:'); 
-  ReadLn(a);
-
-  Write('Podaj wartość b:'); 
-  ReadLn(b);
-
-  Write('Podaj wartość c:'); 
-  ReadLn(c);
-
-  delta := b*b - 4*a*c;
-
-    if delta < 0 then
-      WriteLn('Wyznacznik jest ujemny, brak pierwiastków rzeczywistych.')
-    else
-        if delta = 0 then
-          begin
-          	x1 := -(b/(2*a));
-            WriteLn('x1 to: ', x1:12:4)
-          end
-          
-        else      
-         begin
-          x1 := (-b + sqrt(delta))/(2*a);
-          x2 := (-b - sqrt(delta))/(2*a);
-          WriteLn('x1 to: ', x1:12:4);
-          WriteLn('x2 to: ', x2:12:4);            	
-         end;
-
-
-  ReadLn;
+  
+  choinka(20);
+    
 end.
